6fe35c219bf9cd205ab3ad78b5a81f695ba3886d
Yuliya Simonchyk <y.simonchyk@softteco.com>
2023-12-12 10:25:33 +0300

    del:header

--- client/pages/home/home.tsx
+++ client/pages/home/home.tsx
@@ -1,10 +1,8 @@
-import Header from '@components/header/header';
 import HomeContent from '@components/home-content/home-content';
 
 export default function Home() {
     return (
         <>
-            <Header />
             <HomeContent />
         </>
     );
