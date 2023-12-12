e253e8bda84befee86997f881e45e65e1939a578
Yuliya Simonchyk <y.simonchyk@softteco.com>
2023-12-12 10:04:18 +0300

    fix:husky

--- client/next.config.js
+++ client/next.config.js
@@ -1,18 +1,23 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
+const path = require('path');
 
 const nextConfig = {
-  webpack: (config) => {
-    config.externals = [...config.externals, { canvas: "canvas" }]; // required to make Konva & react-konva work
-    return config;
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-  },
-  reactStrictMode: true,
-  env: {
-    GENERATOR_URL: process.env.GENERATOR_URL,
-  },
+    webpack: config => {
+        config.externals = [...config.externals, { canvas: 'canvas' }]; // required to make Konva & react-konva work
+        return config;
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+    reactStrictMode: true,
+    env: {
+        GENERATOR_URL: process.env.GENERATOR_URL,
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 };
 
 module.exports = nextConfig;
