6897254a1bbd461265ff6f9a41a368c72319c858
Yuliya Simonchyk <y.simonchyk@softteco.com>
2023-12-19 12:01:12 +0300

    update:validation, del react-query

--- client/pages/generator.tsx
+++ client/pages/generator.tsx
@@ -1,9 +1,7 @@
 import Head from 'next/head';
 import { NextUIProvider } from '@nextui-org/react';
 import Gen from '@components/generator-content/generator-content';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import '../styles/globals.css';
-const queryClient = new QueryClient();
 
 export default function Generator() {
     return (
@@ -11,11 +9,9 @@ export default function Generator() {
             <Head>
                 <title>Generator tool</title>
             </Head>
-            <QueryClientProvider client={queryClient}>
-                <div className="w-screen h-screen flex items-start justify-center  text-foreground bg-background m-top-4">
-                    <Gen />
-                </div>
-            </QueryClientProvider>
+            <div className="w-screen h-screen flex items-start justify-center  text-foreground bg-background m-top-4">
+                <Gen />
+            </div>
         </NextUIProvider>
     );
 }
--- client/src/app/components/generator-content/validation-helper.tsx
+++ client/src/app/components/generator-content/validation-helper.tsx
@@ -1,4 +1,4 @@
 export const isValidProjectName = projectName => {
-    const regex = /^[a-zA-Z0-9]+$/;
+    const regex = /^[a-zA-Z0-9-]+$/;
     return regex.test(projectName);
 };
